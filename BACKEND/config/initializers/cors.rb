6fbc00eecab5c95d39c24942cd38cfb6be81860d
BrunoEPaez <emmanuelnovo007@gmail.com>
2026-02-02 14:23:22 -0300

    Update: api client con variables de entorno

--- BACKEND/config/initializers/cors.rb
+++ BACKEND/config/initializers/cors.rb
@@ -1,13 +1,9 @@
-# config/initializers/cors.rb
-
+# BACKEND/config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Permite que tu frontend de Cloudflare se conecte
-    origins "https://01715bc7.talenthub-uqg.pages.dev"
-
-    resource "*",
+    origins '*' # Esto permite CUALQUIER origen
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
